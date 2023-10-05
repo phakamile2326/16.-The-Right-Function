432693c4651ed7c9260803d2367d0b2007c7a8e0
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 02:03:19 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.Click
+
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Dim MyText As String
+        MyText = "Visual Basic"
+
+        MsgBox(Microsoft.VisualBasic.Right(MyText, 4))
+
+    End Sub
+
+
+
+End Class
